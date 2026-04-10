27b4782c136b60eb38324e87074ef3cabab7f755
knightromantic <jiepiguo@gmail.com>
2026-04-10 14:44:14 +0800

    fix: correct Clarity script injection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Header } from "@/components/Header";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "DiaryFlow — 记录生活，段落留声",
@@ -17,6 +18,15 @@ export default function RootLayout({
       <body>
         <Header />
         <main>{children}</main>
+		 <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w9dsbhewxz");
+          `}
+        </Script>
       </body>
     </html>
   );
